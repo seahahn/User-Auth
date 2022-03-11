b28d7a202f5ace667d226aa7d379613a3438b625
Library-of-jade <minseok1335@gmail.com>
2022-03-11 10:21:49 +0900

    clear

--- base/settings.py
+++ base/settings.py
@@ -163,3 +163,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = 'AI.PLAY.PROJECT0001@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwer1234+'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
